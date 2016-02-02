26bf601729f17047d3b4ca932dadf16fde53a0df
hand <hand@chuchujie.com>
2016-02-02 12:19:48 +0800

    sss

---
+++ system/caches/caches_log/admin.log.2016-02-02.php
@@ -0,0 +1,11 @@
+<?php exit; ?> 
+
+
+
+2016-02-02 12:18:59------>array (
+  0 => 'UPDATE `go_admin` SET `logintime`=\'1454386739\' WHERE (`uid`=\'1\')',
+  1 => 'UPDATE `go_admin` SET `loginip`=\'127.0.0.1\' WHERE (`uid`=\'1\')',
+) 
+
+
+
