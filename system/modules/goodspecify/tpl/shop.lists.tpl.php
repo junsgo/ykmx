a37a6543a609f2964303aee3bd9bef8bd075e6af
hand <hand@chuchujie.com>
2016-02-17 11:49:51 +0800

    1

--- system/modules/goodspecify/tpl/shop.lists.tpl.php
+++ system/modules/goodspecify/tpl/shop.lists.tpl.php
@@ -219,7 +219,7 @@ tr{ text-align:center}
 				</tr>
         </thead>
         <tbody>
-        <?php print_r($shoplist);?>				
+        <?php print_r($shoplist);?>	 			
         	<?php foreach($shoplist as $v) { ?>
             <tr>
               <td align='center'><input name='listorders[<?php echo $v['id']; ?>]' type='text' size='3' value='<?php echo $v['order']; ?>' class='input-text-c'></td>  
